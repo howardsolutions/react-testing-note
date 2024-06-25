b9d96b1eee0b4da54034b80b095ad1eb3b15cefc
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-25 16:31:46 +0700

    testing: entire overview app

--- 1-overview/src/App.test.js
+++ 1-overview/src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import user from '@testing-library/user-event';
 
-test.skip('renders learn react link', () => {
+test('can receive a new user and show it on a list', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+
+  const button = screen.getByRole('button');
+
+  user.click(nameInput);
+  user.keyboard('jane');
+
+  user.click(emailInput);
+  user.keyboard('jane@email.com');
+
+  user.click(button);
+
+  const name = screen.getByRole('cell', { name: 'jane' });
+  const email = screen.getByRole('cell', { name: 'jane@email.com' });
+
+  expect(name).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
 });
