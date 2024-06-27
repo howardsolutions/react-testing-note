386be7e44af0d765d9da54cb7bc10c99be1709c3
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-27 15:33:46 +0700

    chore: doing msw setup

--- 2-codesplain/src/routes/HomeRoute.test.js
+++ 2-codesplain/src/routes/HomeRoute.test.js
@@ -4,3 +4,32 @@ import { rest } from 'msw';
 
 import { MemoryRouter } from 'react-router';
 import HomeRoute from './HomeRoute';
+
+const handlers = [
+  rest.get('api/repositories', (req, res, ctx) => {
+    const query = req.url.searchParams('q');
+
+    return res(
+      ctx.json({
+        items: [
+          { id: 1, full_name: 'Full name!' },
+          { id: 2, full_name: 'Other name' },
+        ],
+      })
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+  server.listen();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
