6079c6d9932e0b43fbc515fab2f33b068ed9c4b3
thinhphungdev <phungthinh0946519356@gmail.com>
2024-06-28 09:27:42 +0700

    test: write HomeRoute effective data fetching test

--- 2-codesplain/src/routes/HomeRoute.test.js
+++ 2-codesplain/src/routes/HomeRoute.test.js
@@ -1,35 +1,64 @@
 import { render, screen } from '@testing-library/react';
-import { setupServer } from 'msw/node';
 import { rest } from 'msw';
-
-import { MemoryRouter } from 'react-router';
+import { setupServer } from 'msw/node';
+import { MemoryRouter } from 'react-router-dom';
 import HomeRoute from './HomeRoute';
 
 const handlers = [
-  rest.get('api/repositories', (req, res, ctx) => {
-    const query = req.url.searchParams('q');
+  rest.get('/api/repositories', (req, res, ctx) => {
+    const language = req.url.searchParams.get('q').split('language:').at(1);
+    // console.log(language);
 
     return res(
       ctx.json({
         items: [
-          { id: 1, full_name: 'Full name!' },
-          { id: 2, full_name: 'Other name' },
+          { id: 1, full_name: `${language}_one` },
+          { id: 2, full_name: `${language}_two` },
         ],
       })
     );
   }),
 ];
-
 const server = setupServer(...handlers);
 
 beforeAll(() => {
   server.listen();
 });
-
 afterEach(() => {
   server.resetHandlers();
 });
-
 afterAll(() => {
   server.close();
+});
+
+test('renders two links for each language', async () => {
+  render(
+    <MemoryRouter>
+      <HomeRoute />
+    </MemoryRouter>
+  );
+
+  // Loop over each language
+  const languages = [
+    'javascript',
+    'java',
+    'go',
+    'rust',
+    'python',
+    'typescript',
+  ];
+
+  // For each language, make sure we see two links
+  for (let language of languages) {
+    const links = await screen.findAllByRole('link', {
+      name: new RegExp(`${language}_`),
+    });
+
+    expect(links).toHaveLength(2);
+    expect(links[0]).toHaveTextContent(`${language}_one`);
+    expect(links[1]).toHaveTextContent(`${language}_two`);
+    expect(links[0]).toHaveAttribute('href', `/repositories/${language}_one`);
+    expect(links[1]).toHaveAttribute('href', `/repositories/${language}_two`);
+  }
+  // Assert that the links have the appropriate full_name
 });
